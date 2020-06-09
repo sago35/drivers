3a2d6b7ec170e69e5f83a17ced8c2d3ab4a1ee27
sago35 <sago35@gmail.com>
2020-06-10 08:24:43 +0900

    Update

--- rtl8720dn/espat.go
+++ rtl8720dn/espat.go
@@ -20,17 +20,20 @@ package rtl8720dn
 
 import (
 	"errors"
+	"fmt"
 	"machine"
 	"strconv"
 	"strings"
 	"time"
-
-	"tinygo.org/x/drivers/net"
 )
 
 // Device wraps UART connection to the ESP8266/ESP32.
 type Device struct {
-	bus machine.UART
+	bus       machine.SPI
+	chipPu    machine.Pin
+	syncPin   machine.Pin
+	csPin     machine.Pin
+	uartRxPin machine.Pin
 
 	// command responses that come back from the ESP8266/ESP32
 	response []byte
@@ -39,18 +42,66 @@ type Device struct {
 	socketdata []byte
 }
 
+type Config struct {
+}
+
 // ActiveDevice is the currently configured Device in use. There can only be one.
 var ActiveDevice *Device
 
 // New returns a new espat driver. Pass in a fully configured UART bus.
-func New(b machine.UART) *Device {
-	return &Device{bus: b, response: make([]byte, 512), socketdata: make([]byte, 0, 1024)}
+func New(bus machine.SPI, chipPu, syncPin, csPin, uartRxPin machine.Pin) *Device {
+	return &Device{
+		bus:       bus,
+		chipPu:    chipPu,
+		syncPin:   syncPin,
+		csPin:     csPin,
+		uartRxPin: uartRxPin,
+
+		response:   make([]byte, 512),
+		socketdata: make([]byte, 0, 1024),
+	}
 }
 
 // Configure sets up the device for communication.
-func (d Device) Configure() {
-	ActiveDevice = &d
-	net.ActiveDevice = ActiveDevice
+func (d *Device) Configure(config *Config) error {
+	ActiveDevice = d
+	// Reset SPI slave device(RTL8720D)
+	d.chipPu.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	d.chipPu.Low()
+
+	// start the SPI library:
+	// Start SPI transaction at a quarter of the MAX frequency
+	// -> SPI is already configured
+
+	// initalize the  data ready and chip select pins:
+	d.syncPin.Configure(machine.PinConfig{Mode: machine.PinInput})
+	d.csPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	d.csPin.High()
+
+	// When RTL8720D startup, set pin UART_LOG_TXD to lowlevel
+	// will force the device enter UARTBURN mode.
+	// Explicit high level will prevent above things.
+	d.uartRxPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	d.uartRxPin.High()
+
+	// reset duration
+	time.Sleep(20 * time.Millisecond)
+
+	// Release RTL8720D reset, start bootup.
+	d.chipPu.High()
+
+	// give the slave time to set up
+	time.Sleep(500 * time.Millisecond)
+	d.uartRxPin.Configure(machine.PinConfig{Mode: machine.PinInput})
+
+	r, err := d.Response(1000)
+	if err != nil {
+		fmt.Printf("error : %s\r\n", err.Error())
+		return err
+	}
+	fmt.Printf("%s\r\n", string(r))
+
+	return nil
 }
 
 // Connected checks if there is communication with the ESP8266/ESP32.
@@ -67,12 +118,14 @@ func (d *Device) Connected() bool {
 
 // Write raw bytes to the UART.
 func (d *Device) Write(b []byte) (n int, err error) {
-	return d.bus.Write(b)
+	//return d.bus.Write(b)
+	return d.at_spi_write(b)
 }
 
 // Read raw bytes from the UART.
 func (d *Device) Read(b []byte) (n int, err error) {
-	return d.bus.Read(b)
+	//return d.bus.Read(b)
+	return 0, nil
 }
 
 // how long in milliseconds to pause after sending AT commands
@@ -157,12 +210,19 @@ func (d *Device) Response(timeout int) ([]byte, error) {
 	pause := 100 // pause to wait for 100 ms
 	retries := timeout / pause
 
+	var err error
 	for {
-		size = d.bus.Buffered()
+		size, err = d.at_spi_read(d.response[start:])
+		if err != nil {
+			return nil, err
+		}
 
 		if size > 0 {
 			end += size
-			d.bus.Read(d.response[start:end])
+
+			if strings.Contains(string(d.response[:end]), "ready") {
+				return d.response[start:end], nil
+			}
 
 			// if "+IPD" then read socket data
 			if strings.Contains(string(d.response[:end]), "+IPD") {
@@ -218,5 +278,6 @@ func (d *Device) parseIPD(end int) error {
 
 // IsSocketDataAvailable returns of there is socket data available
 func (d *Device) IsSocketDataAvailable() bool {
-	return len(d.socketdata) > 0 || d.bus.Buffered() > 0
+	//return len(d.socketdata) > 0 || d.bus.Buffered() > 0
+	return false
 }
