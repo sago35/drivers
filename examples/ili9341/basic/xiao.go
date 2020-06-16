28f54cf6f75d05cbecb29ef80895898630de57dd
sago35 <sago35@gmail.com>
2020-06-17 08:19:59 +0900

    Add ili9341 spidriver of atsamd2x

---
+++ examples/ili9341/basic/xiao.go
@@ -0,0 +1,30 @@
+// +build xiao
+
+package main
+
+import (
+	"machine"
+
+	"tinygo.org/x/drivers/ili9341"
+)
+
+var (
+	display = ili9341.NewSpi(
+		machine.SPI0,
+		machine.D0,
+		machine.D1,
+		machine.D2,
+	)
+
+	backlight = machine.D3
+	configErr error
+)
+
+func init() {
+	configErr = machine.SPI0.Configure(machine.SPIConfig{
+		SCK:       machine.SPI0_SCK_PIN,
+		MOSI:      machine.SPI0_MOSI_PIN,
+		MISO:      machine.SPI0_MISO_PIN,
+		Frequency: 24000000,
+	})
+}
