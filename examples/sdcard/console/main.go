3b245e91a865fd1a685e3d5e2369b3f3f97696ad
sago35 <sago35@gmail.com>
2020-07-08 20:49:42 +0900

    Change to per-board settings

--- examples/sdcard/console/main.go
+++ examples/sdcard/console/main.go
@@ -7,24 +7,24 @@ import (
 	"tinygo.org/x/drivers/sdcard"
 )
 
+var (
+	spi    machine.SPI
+	csPin  machine.Pin
+	ledPin machine.Pin
+)
+
 func main() {
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	machine.SPI0.Configure(machine.SPIConfig{
-		SCK:       machine.SPI0_SCK_PIN,
-		MOSI:      machine.SPI0_MOSI_PIN,
-		MISO:      machine.SPI0_MISO_PIN,
-		Frequency: 24000000,
-		LSBFirst:  false,
-		Mode:      0, // phase=0, polarity=0
-	})
-	sd := sdcard.New(machine.SPI0, machine.D4)
+	sd := sdcard.New(spi, csPin)
 
 	go RunFor(&sd)
 
 	for {
-		led.Toggle()
+		led.High()
+		time.Sleep(200 * time.Millisecond)
+		led.Low()
 		time.Sleep(200 * time.Millisecond)
 	}
 }
